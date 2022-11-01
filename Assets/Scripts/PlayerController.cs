a96ab21a627dcc35e3396b4454edf5a55b8173be
Jeremy Dahlberg <34037966+Jerster101@users.noreply.github.com>
2022-11-01 10:39:42 -0700

    Base game funictionality
    Added 3 packages for multiplayer support.  Also have a basis for multiplayer and game functionality

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    //Remove player controls if this is not my player
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) enabled = !enabled;
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
